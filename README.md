8d7d3fb5d6acfaae9985e7032e2fb8b64a0ede77
Movits <robertomovits@gmail.com>
2023-10-31 11:05:42 -0300

    agregue distintas rutas

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 5. en package.json
     - "dev": "nodemon index.js"
     - "type": "module"
+6. inicializar express
+7. app.listen para iniciar servidor
+8. app.use para usar una ruta por defecto
+    - res.send para enviar info a la pagina
+9. app.get("/1") para def una otra ruta
+
--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.get("", (req,res)=>{
+    res.json({atributo: "<h1> Hola! </h1>"})
+});
+
+app.get("/api", (req,res)=>{
+    res.send("<h1> Hola 2! </h1>")
+});
+
+app.get("/api/:id", (req,res)=>{
+    console.log(req.params);
+    res.send(`<h1> Hola 3! ${req.params.id} </h1>`)
+});
+
+
 app.listen(3000, ()=>{
-    console.log("servidor activo");
-})
+    console.log("servidor activo")
+});
+
+app.use((req,res)=>{
+    res.send("<h1> ERROR: ruta no encontrada </h1>")
+});
