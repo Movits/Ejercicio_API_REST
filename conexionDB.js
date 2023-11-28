c5c7723f312ec77a1bbaf35364355532531af520
Movits <robertomovits@gmail.com>
2023-11-28 00:27:00 -0300

    agregue query para crear tabla

--- conexionDB.js
+++ conexionDB.js
@@ -7,6 +7,16 @@ console.log(configuracion.USER);
 console.log(configuracion.PASSWORD);
 console.log(configuracion.NAME); */
 
+const CREAR_TABLA = `
+    CREATE TABLE IF NOT EXISTS usuarios (
+        dni INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(45) NOT NULL,
+        edad INT NOT NULL,
+        email VARCHAR(45) NOT NULL,
+        contrasena VARCHAR(45) NOT NULL
+    );
+`;
+
 const pool = createPool({
     host : configuracion.HOST,
     port : configuracion.PORT,
@@ -14,6 +24,8 @@ const pool = createPool({
     password : configuracion.PASSWORD,
     database : configuracion.NAME,
 });
+
+await pool.query(CREAR_TABLA);
 
 console.log("conexion exitosa");
 
